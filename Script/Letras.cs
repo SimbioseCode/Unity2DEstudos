79c8669332977cc9bd55530660750fbf2cb4e288
pabloverly <PVPierre@scmv.corp>
2021-04-14 14:53:05 -0300

    Primeiro atualizado

--- Script/Letras.cs
+++ Script/Letras.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+   //https://www.youtube.com/watch?v=EohhGbRgNds
 [RequireComponent(typeof(Rigidbody2D))]
 public class Letras : MonoBehaviour //FUNCIONA APENAS COM CAMERA ORTOGRAFICAS
 {
@@ -9,6 +10,7 @@ public class Letras : MonoBehaviour //FUNCIONA APENAS COM CAMERA ORTOGRAFICAS
     Vector3 posDestino;
     Vector3 vetorDirecao;
     Rigidbody2D _rigidbody2D;
+    bool estaArrastando;
     float distancia;
 
     [HideInInspector]//deixa publica mas nao aparece no inspectro 
@@ -24,15 +26,28 @@ public class Letras : MonoBehaviour //FUNCIONA APENAS COM CAMERA ORTOGRAFICAS
     public float distanciaMinimaConector = 0.5f;
     bool Arratando;
 
-    void Start()
-    {
+    void Start() {
       _rigidbody2D = transform.root.GetComponent<Rigidbody2D>(); //pega o rigidbody do objeto pai   
-      _rigidbody2D.gravityScale = 1;
+      _rigidbody2D.gravityScale = 1; //desativar a gravidade
     }
 
-    private void OnMouseDown()
-    {
-        
+    void OnMouseDown(){
+        posInicial = transform.root.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _rigidbody2D.gravityScale = 0;
+        estaArrastando = true;
+        estaConectado = false;
+        //Debug.Log("teste");
     }
-   
+
+    void OnMouseDrag()  {
+         posDestino =  posInicial +  Camera.main.ScreenToWorldPoint(Input.mousePosition);
+         vetorDirecao = posDestino - transform.root.position;
+         _rigidbody2D.velocity = vetorDirecao * velocidadeMov;
+    }
+    private void OnMouseUp()
+    {
+        _rigidbody2D.gravityScale = 1;
+        estaArrastando = false;        
+    }
+
 }
